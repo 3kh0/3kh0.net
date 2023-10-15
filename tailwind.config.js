f8ff5d65bbb24afee688d818970c3f06cc0eaea8
3kh0 <echo-the-coder@tuta.io>
2023-10-15 17:18:08 -0400

    Ok we need this opss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
