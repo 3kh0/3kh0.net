172b0bf27b1af6316d49650f582e14fa16f75a88
3kh0 <echo-the-coder@tuta.io>
2024-01-20 23:18:53 -0500

    stuffs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,3 +20,24 @@ console.log(
       fox gang :D⠀- 3kh0`,
   "color: #fc9505;"
 );
+
+// Place headers on pages that don't have them
+window.onload = function() {
+  var headerDiv = document.getElementById('header');
+  if (headerDiv) {
+    headerDiv.innerHTML = `
+      <header class="py-4 px-8 flex justify-between items-center max-w-6xl mx-auto">
+        <div>
+          <h1 class="text-7xl font-bold text-green-500 text-center py-1">3kh0</h1>
+          <p class="text-sm">Programmer, Hacker, Silly fox</p>
+        </div>
+        <div>
+          <a href="#projects" class="button"><i class="fas fa-project-diagram"></i> Projects</a>
+          <a href="#art" class="button"><i class="fas fa-palette"></i> Art</a>
+          <a href="#connect" class="button"><i class="fas fa-share-alt"></i> Socials</a>
+          <a href="/extra/index.html"><i class="fas fa-star"></i> Extras</a>
+        </div>
+      </header>
+    `;
+  }
+}
