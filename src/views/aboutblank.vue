7f7dca56bf4863e3541e29eae0b5d624f540a847
Echo <echo-the-coder@tuta.io>
2024-03-04 12:12:16 -0500

    Add script back

--- src/views/aboutblank.vue
+++ src/views/aboutblank.vue
@@ -1,3 +1,29 @@
+<script>
+export default {
+  methods: {
+    createPage() {
+      var url = document.getElementById("url-target");
+      if (url.value.substring(0, 8) !== "https://" && url.value.substring(0, 7) !== "http://") {
+        url.value = "https://" + url.value.split("https://").pop();
+      } else if (url.value.substring(0, 7) == "http://") {
+        url.value = "https://" + url.value.split("http://").pop();
+      }
+      var win = window.open();
+      win.document.body.style.margin = "0";
+      win.document.body.style.height = "100vh";
+      var frm = win.document.createElement("iframe");
+      frm.style.border = "none";
+      frm.style.width = "100%";
+      frm.style.height = "100%";
+      frm.style.margin = "0";
+      frm.referrerpolicy = "no-referrer";
+      frm.allow = "fullscreen";
+      frm.src = url.value;
+      win.document.body.appendChild(frm);
+    },
+  },
+};
+</script>
 <template>
   <main class="max-w-6xl mx-auto px-4">
     <h2 class="text-3xl mb-4">About Blank</h2>
