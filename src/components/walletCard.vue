2a24f934cc12a3da580c6194b31f264c528a6c26
Echo <echo-the-coder@tuta.io>
2024-03-16 10:32:25 -0400

    Move images

--- src/components/walletCard.vue
+++ src/components/walletCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wallet mt-4 bg-gray-800 p-4 rounded-xl shadow-lg flex-grow flex-shrink min-w-0">
     <h3 class="text-2xl mb-2">{{ wallet.name }}</h3>
-    <img :src="`/img/${wallet.img}.png`" :alt="`${wallet.name} Wallet QR Code`" class="mb-2 rounded-xl" />
+    <img :src="`/img/crypto/${wallet.img}.png`" :alt="`${wallet.name} Wallet QR Code`" class="mb-2 rounded-xl" />
     <p :id="`${wallet.img}-address`" class="wallet-address w-full whitespace-pre-wrap font-mono text-center">{{ trimAddress(wallet.address) }}</p>
     <button v-on:click="copyAdr(wallet.address)" class="w-full mt-2 bg-blue-500 hover:bg-blue-700 transition duration-300 text-white py-2 px-4 rounded"><i class="fas fa-copy"></i> Copy address</button>
     <a :href="wallet.explorerLink + wallet.address" target="_blank">
